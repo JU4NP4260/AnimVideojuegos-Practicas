f607dc871c881c274c6a45643f2e515754daba4b
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2024-02-05 19:11:12 -0500

    Walking Alien

--- Assets/Scripts/AnimationControllerJP.cs
+++ Assets/Scripts/AnimationControllerJP.cs
@@ -7,11 +7,36 @@ using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class AnimationControllerJP : MonoBehaviour
 {
+    private Animator animator;
+    private Vector2 currentInput;
+    private Vector2 nextInput;
+    private Vector2 inputVelocity;
+
+    private int motionXID, motionYID;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        motionXID = Animator.StringToHash("MoveX");
+        motionYID = Animator.StringToHash("MoveY");
+    }
+
+    private void Update()
+    {
+        if (animator != null)
+        {
+            currentInput = Vector2.SmoothDamp(currentInput, nextInput, ref inputVelocity, 0.5f);
+            animator.SetFloat(motionXID, currentInput.x);
+            animator.SetFloat(motionYID, currentInput.y);
+        }
+    }
 
     public void Move(CallbackContext context)
     {
+
         Vector2 motionValue = context.ReadValue<Vector2>();
         Debug.Log(motionValue);
+        nextInput = motionValue;
     }
 
 }
